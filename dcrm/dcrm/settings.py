cbf8d63a060ca0a11e75696634c8205907584224
Jordan Rammelsberg <96778376+Jramm91@users.noreply.github.com>
2023-04-09 18:22:45 +0000

    started building login page

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://jramm91-friendly-doodle-qrr9qvww6j3x444-8000.preview.app.github.dev']
