46ba7ea75325c1705243a449112ce8a34d7acbde
Jordan Rammelsberg <96778376+Jramm91@users.noreply.github.com>
2023-04-12 14:59:54 +0000

    Created login form

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -5,10 +5,9 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
+    
     return render(request, 'home.html', {})
 
-def login_user(request):
-    pass
 
 def logout_user(request):
     pass
