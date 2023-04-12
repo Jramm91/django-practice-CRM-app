1ba40a60ea863f1a9fecfc39914d6cdaea4c3ba1
Jordan Rammelsberg <96778376+Jramm91@users.noreply.github.com>
2023-04-12 15:56:43 +0000

    added register user function to render register.html

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -26,3 +26,7 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You Have Been Logged Out Successfully.")
     return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html')
+    
