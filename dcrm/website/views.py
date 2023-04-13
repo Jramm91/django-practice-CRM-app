3a1be246b9219aaab64af0769ed50995a5c1a13d
Jordan Rammelsberg <96778376+Jramm91@users.noreply.github.com>
2023-04-13 14:49:10 +0000

    created customer record view

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -11,42 +11,53 @@ def home(request):
     records = Record.objects.all()
 
     # check to see if logging in
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
         # Authenticate
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, "You Have Been Logged In Successfully!")
-            return redirect('home')
+            return redirect("home")
         else:
             messages.error(request, "There Was an Error Logging In.")
-            return redirect('home')
+            return redirect("home")
     else:
-        return render(request, 'home.html', {'records':records})
+        return render(request, "home.html", {"records": records})
 
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You Have Been Logged Out Successfully.")
-    return redirect('home')
+    return redirect("home")
 
 
 def register_user(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             # Authenticate and login
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(
-                request, "You Have Successfully Registered Account")
-            return redirect('home')
+            messages.success(request, "You Have Successfully Registered Account")
+            return redirect("home")
     else:
         form = SignUpForm()
-        return render(request, 'register.html', {'form': form})
-    return render(request, 'register.html', {'form': form})
+        return render(request, "register.html", {"form": form})
+    return render(request, "register.html", {"form": form})
+
+
+def customer_record(request, pk):
+    # check if user is logged in to view customer records
+    if request.user.is_authenticated:
+        # look up records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, "record.html", {"customer_record": customer_record})
+
+    else:
+        messages.success(request, "Please login to view customer record.")
+        return redirect("home")
